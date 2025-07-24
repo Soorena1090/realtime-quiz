7a7bbab3b9ca340165a84b2c4eb29ac70652ec86
soorena84 <soorenamahbobi@gmail.com>
2025-07-24 10:09:53 +0330

    feat: create database connection

--- src/model/userModel.js
+++ src/model/userModel.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
-    name: {type: String, require: true},
-    email: {type: String, require:true, unique: true},
-    password: { type: String, require:true}
+    name: {type: String, required: true},
+    email: {type: String, required:true, unique: true},
+    password: { type: String, required:true}
 });
 
 const User = mongoose.model('User', userSchema);
--- src/server.js
+++ src/server.js
@@ -1,27 +1,15 @@
 const http = require('http');
 const WebSocket = require('ws');
 const app = require('./index');
-const updateController = require('./controller/updateController');
+const updateController = require('./controllers/updateController');
+const connectDB = require('./database/dbConnect')
 
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
 updateController.setWssInstance(wss);
 
-wss.on('connection', ws => {
-  console.log('WebSocket connected');
-  ws.send(JSON.stringify({ message: 'Welcome!' }));
-});
-
-app.get('/long-polling', (req, res) => {
-  updateController.addWaitingClient(res);
-  
-  req.setTimeout(30000, () => {
-    res.json({ message: 'Timeout - no new data', timestamp: new Date() });
-  });
-});
-
-app.post('/update', updateController.updateData);
+connectDB();
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
