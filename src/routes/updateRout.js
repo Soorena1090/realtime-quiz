a86d4f878770a4a53189b3babcd739aad91bcfae
soorena84 <soorenamahbobi@gmail.com>
2025-07-24 12:14:59 +0330

    feat: fix some buge on authvontroller and .env

--- src/routes/updateRout.js
+++ src/routes/updateRout.js
@@ -6,7 +6,7 @@ router.post('/update', updateController.sendUpdate);
 router.get('/long-polling', (req, res) => {
   updateController.addClient(res);
 
-  req.setTimeout(30000, () => {
+  req.setTimeout(10000, () => {
     res.json({ message: 'Timeout - no new data', timestamp: new Date() });
   });
 });
