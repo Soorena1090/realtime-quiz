60085cb4bb8126c2a7fe86bb9b25d75265b64984
soorena84 <soorenamahbobi@gmail.com>
2025-07-24 14:10:03 +0330

    feat: cashing with redis for return all users ins users api

--- src/controllers/authcontroller.js
+++ src/controllers/authcontroller.js
@@ -17,7 +17,7 @@ exports.register = async (req, res) => {
       email: email,
       password: hashed,
     });
-    await newUser.save();
+
     res.status(201).json({ message: "register done" });
   } catch (err) {
     console.error(err);
