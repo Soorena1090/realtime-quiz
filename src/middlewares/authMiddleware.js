c769498fbb92e98b72c47ebd162a4d4a6cc0ebb6
soorena84 <soorenamahbobi@gmail.com>
2025-07-24 15:55:01 +0330

    fix: fix the JWT token in middleware

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+
 const authMiddleware = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   if (!authHeader)
@@ -6,11 +7,13 @@ const authMiddleware = (req, res, next) => {
 
   const token = authHeader.split(" ")[1];
   if (!token) return res.status(401).json({ message: "Token missing" });
+
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded.id;
     next();
-  } catch {
+  } catch (err) {
+    console.error('JWT ERROR',err.message )
     return res.status(401).json({ message: " invalid token" });
   }
 };
