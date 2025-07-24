776ea51ae646cbd26c749e9d5168c37f9db20190
soorena84 <soorenamahbobi@gmail.com>
2025-07-24 11:25:08 +0330

    feat: add test for endpoints and database but

--- src/controllers/authcontroller.js
+++ src/controllers/authcontroller.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const User = require('../models/userModel');
+const User = require('../model/userModel');
 
 exports.register = (req, res) => {
   const { name, email, password } = req.body;
--- src/controllers/ping.controller.js
+++ src/controllers/ping.controller.js
@@ -1,3 +1,3 @@
 exports.ping = (req,res) => {
-    res.status(200).json({message: 'okr'});
+    res.status(200).json({message: 'oky'});
 };
